8c93d36e4dbe046994164fc7ac0afdcf6648fbf1
Daniele Scasciafratte <mte90net@gmail.com>
2021-07-14 11:56:41 +0200

    feat(release): first release?

--- finder.php
+++ finder.php
@@ -2,6 +2,12 @@
 
 return \StubsGenerator\Finder::create()
     ->in('source/genesis/lib/functions')
+    ->append(\StubsGenerator\Finder::create()
+            ->in(['source/genesis/lib'])
+            ->files()
+            ->depth('< 1')
+            ->path('framework.php')
+      )
     ->notPath('compat.php')
     ->notPath('deprecated.php')
     ->sortByName()
